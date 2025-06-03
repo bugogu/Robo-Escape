b417ecc05588f188e95f76ba3d43a06123f4e21a
Oğuzhan Sahin <bey.ogz.han@gmail.com>
2025-06-03 10:05:58 +0300

    Menu UI Fix and Upgrades

--- Robo Escape/Assets/Base/_Scripts/Main/MenuUI.cs
+++ Robo Escape/Assets/Base/_Scripts/Main/MenuUI.cs
@@ -16,23 +16,23 @@ public class MenuUI : MonoSingleton<MenuUI>
 
     [Header("GameObjects")]
     [SerializeField] private GameObject _escapeButton;
-    [SerializeField] private GameObject _upgradesCanvas, _settingsPanel;
+    [SerializeField] private GameObject _upgradesCanvas, _infoPanel, _settingsPanel;
 
     [Header("Transforms")]
     [SerializeField] private RectTransform _capacityRect;
     [SerializeField] private RectTransform _handRect;
-    
+
     [Header("Floats")]
     [SerializeField] private float _showDuration = 1f;
     [SerializeField] private float _closeDuration = 3f;
     [SerializeField] private float _handRectMoveDuration = 1f;
-    
+
     [Header("Others")]
     [SerializeField] private Button _playButton;
     [SerializeField] private Settings _settings;
     [SerializeField] private Vector3 _handRectYThreshold = new Vector3(-140, 160, 0);
     [SerializeField] private Canvas _infoCanvas;
-    
+
     #endregion
 
     private bool _isShowed = false;
@@ -43,24 +43,24 @@ public class MenuUI : MonoSingleton<MenuUI>
             StartHandAnimation();
         else
             SetLevelText();
-       
-       _protocolText.text = $"{PlayerPrefs.GetInt(Consts.Prefs.PROTOCOLCOUNT, 0)}";
 
-       Settings.Instance.SetOutlines(Settings.Instance.Outlines == 1);
+        _protocolText.text = $"{PlayerPrefs.GetInt(Consts.Prefs.PROTOCOLCOUNT, 0)}";
 
-       _playButton.onClick.AddListener(PlayButton);
+        Settings.Instance.SetOutlines(Settings.Instance.Outlines == 1);
+
+        _playButton.onClick.AddListener(PlayButton);
     }
 
-    private void Update() 
+    private void Update()
     {
-        if(!Input.GetMouseButtonDown(0)) return;
+        if (!Input.GetMouseButtonDown(0)) return;
 
-        if(!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, 100, LayerMask.GetMask("Player"))) return;
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, 100, LayerMask.GetMask("Player"))) return;
 
-        if(_settingsPanel.transform.localScale == Vector3.one || _upgradesCanvas.activeSelf) return;
+        if (ActivePanelsCheck()) return;
 
         ShowEnergyCapacity();
-         
+
     }
 
     public void UpgradesCanvasEnabled()
@@ -77,7 +77,7 @@ public class MenuUI : MonoSingleton<MenuUI>
     #region Private Methods
 
     private void StartHandAnimation()
-    { 
+    {
         _handRect.parent.gameObject.SetActive(true);
         _handRect.DOAnchorPos(_handRectYThreshold, _handRectMoveDuration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
     }
@@ -97,14 +97,14 @@ public class MenuUI : MonoSingleton<MenuUI>
         TransitionManager.Instance.PlayTransition(1f);
         Invoke(nameof(LoadLevel), .3f);
 
-    } 
+    }
 
-    private void LoadLevel()=>
+    private void LoadLevel() =>
         UnityEngine.SceneManagement.SceneManager.LoadScene(PlayerPrefs.GetInt("Level", 1) + 1);
 
     private void ShowEnergyCapacity()
     {
-        if(_isShowed) return;
+        if (_isShowed) return;
 
         Settings.Instance.PlayButtonSound();
 
@@ -117,8 +117,23 @@ public class MenuUI : MonoSingleton<MenuUI>
         _capacityText.text = (_gameDesignData.MaxEnergyCapacity + (PlayerPrefs.GetInt(Consts.Prefs.CAPACITY, 0) * _gameDesignData.EnergyCapacityUpgradeAmount)).ToString();
     }
 
-    private void CloseEnergyCapacity()=>
-        _capacityRect.DOScale(Vector3.zero, _showDuration).OnComplete(()=> _isShowed = false);
+    private void CloseEnergyCapacity() =>
+        _capacityRect.DOScale(Vector3.zero, _showDuration).OnComplete(() => _isShowed = false);
+
+    private bool ActivePanelsCheck()
+    { 
+        switch (true)
+        {
+            case true when _upgradesCanvas.activeSelf:
+                return true;
+            case true when _infoPanel.activeSelf:
+                return true;
+            case true when _settingsPanel.transform.localScale == Vector3.one:
+                return true;
+            default:
+                return false;
+        }
+    }
 
     #endregion
 }
